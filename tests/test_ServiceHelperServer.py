b795b1fd508a88f4f3248bafd79fd40f39f25311
FATESAIKOU <qzsecftbhhhh@gmail.com>
2019-03-08 18:22:25 +0800

    Define ServiceHelper tester

--- tests/test_ServiceHelperServer.py
+++ tests/test_ServiceHelperServer.py
@@ -5,18 +5,63 @@ A tester program for ServiceHelperServer.py
 @date  : 03/05/2019
 """
 
-import sys
+import json
 
-sys.path.insert(0, '../src')
 
 def test_UpdateGlobalParameter():
-    assert True
+    action = 'UpdateGlobalParameter'
+    args = {
+        'tolerance_sigma': 0.1,
+        'debug': True
+    }
+
+    result = OpenUrl('http://localhost:8000', action, args)
+
+    assert result['args'] == args
 
 def test_GetSFC():
+    action = 'GetSFC'
+    args = {
+        'request_desc': None
+        # ...
+    }
+
+    result = OpenUrl('http://localhost:8000', action, args)
+
+    # assert result
     assert True
 
 def test_UpdateRL():
+    action = 'UpdateRL'
+    args = {
+        # ...
+    }
+
+    result = OpenUrl('http://localhost:8000', action, args)
+
+    # assert result
     assert True
 
 def test_GetLocalTime():
+    action = 'GetLocalTime'
+    args = {
+        # ...
+    }
+
+    result = OpenUrl('http://localhost:8000', action, args)
+
+    # assert result
     assert True
+
+""" Http toolkits """
+from urllib import request, parse
+def OpenUrl(url_base, action, args):
+    # Pack action & args into query url -> send request
+    result_raw = request.urlopen(url_base
+        + '?action=' + parse.quote(action)
+        + '&args=' + parse.quote(json.dumps(args))
+    ).read()
+
+    # Load result to json & return
+    return json.loads(result_raw)
+
